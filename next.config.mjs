854f0e1fc1b6a76783d92b361348044e93ead9f7
An1-sxd <sameranis50@gmail.com>
2026-02-20 14:16:07 +0100

    using loading / error / not-found pages / + using react suspense

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactCompiler: true,
+//   /* config options here */
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "sqyfnfdqixejekchzicd.supabase.co",
+//         port: "",
+//         pathname: "/storage/v1/object/public/cabin-images/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */      //** 
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://sqyfnfdqixejekchzicd.supabase.co/storage/v1/object/public/cabin-images/**")],
+  },
 };
 
 export default nextConfig;
